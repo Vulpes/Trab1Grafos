a273aa1d26d17e9ec8006b3fd2c4ae8010c3860d
Negan59 <guilherme.oliveira@unesp.br>
2019-06-26 18:46:33 -0300

    update...faltam kruskal e belmon ford

--- GrafosTrabalho.js
+++ GrafosTrabalho.js
@@ -322,7 +322,162 @@ class Grafo{
 		          }
 			}
 		}
-    }
+	}
+	//ordenação topólogica
+	getOrdenacaoTopologica(verticeEscolhido) {
+		
+		const lista = this.getRepresentacao()
+		var quantVertice = this.getQNos()
+		var principal = verticeEscolhido
+		var backtracking = []
+		var buscaProf = []
+		var arrayVertices = []
+		var tempo = 0
+		var pilhaTopologica = []
+
+		//Criação de uma lista de array contendo todos os vertices diferentes do
+		//vertice escolhido pelo usuário
+		for(var i = 0; i < quantVertice; i++) {
+			if(verticeEscolhido != i) arrayVertices.push(i);
+		}
+		/*Estrutura auxiliar de array bidimensional contendo na primeira posição da
+		segunda dimensão um objeto tendo cor, tempo da primeira passagem,
+		tempo da segunda passagem e antecessor. Na segunda posição da mesma dimensão a grafo*/
+		for(var i = 0; i < quantVertice; i++){
+			var obj = {
+				cor: "branco",
+				tempoInicial: "indef",
+				tempoFinal: "indef",
+				antecessor: "indef"
+			}
+
+			buscaProf[i] = []
+			buscaProf[i].push(obj)
+			buscaProf[i].push(lista[i])
+		}
+
+
+		while(arrayVertices.length > 0){
+
+			if(buscaProf[verticeEscolhido][0].cor == "branco"){
+
+				buscaProf[verticeEscolhido][0].cor = "cinza"
+				buscaProf[verticeEscolhido][0].tempoInicial = ++tempo
+				buscaProf[verticeEscolhido][0].antecessor = null
+
+				backtracking.push(verticeEscolhido);
+
+				var verticeSecundario
+				var cont = 0
+				while(backtracking.length > 0){
+
+					if(buscaProf[verticeEscolhido][1][cont] != undefined){
+
+						verticeSecundario = buscaProf[verticeEscolhido][1][cont];
+						cont++
+
+						if(buscaProf[verticeSecundario.vertice][0].cor == "branco"){
+
+							buscaProf[verticeSecundario.vertice][0].cor = "cinza"
+							buscaProf[verticeSecundario.vertice][0].tempoInicial = ++tempo
+							buscaProf[verticeSecundario.vertice][0].antecessor = verticeEscolhido
+							backtracking.push(verticeEscolhido)
+							verticeEscolhido = verticeSecundario.vertice
+						}
+
+
+					}
+					else{
+						buscaProf[verticeEscolhido][0].cor = "preto"
+						let obj = {
+							vertice: verticeEscolhido,
+							adj: lista[verticeEscolhido]
+						}
+						pilhaTopologica.unshift(obj)
+						buscaProf[verticeEscolhido][0].tempoFinal = ++tempo
+						buscaProf[verticeEscolhido][0].antecessor = backtracking.pop()
+						verticeEscolhido = buscaProf[verticeEscolhido][0].antecessor
+						cont = 0
+					}
+				}
+
+			}
+			verticeEscolhido = arrayVertices.shift();
+		}
+
+		buscaProf[principal][0].antecessor = null
+
+		return pilhaTopologica;
+	
+	
+}
+  //ordenação de vértices
+  getOrdenacaoVertices(verticeEscolhido) {
+		
+	let lista = this.getRepresentacao()
+	let quantVertice = this.getQNos()
+	let backtracking = [];
+	let buscaProf = [];
+	let arrayVertices = [];
+	let pilha = []
+
+
+	//Criação de uma lista de array contendo todos os vertices diferentes do
+	//vertice escolhido pelo usuário
+	for(var i = 0; i < quantVertice; i++) {
+		if(verticeEscolhido != i) arrayVertices.push(i);
+	}
+	/*Estrutura auxiliar de array bidimensional contendo na primeira posição da
+	segunda dimensão um objeto tendo cor, tempo da primeira passagem,
+	tempo da segunda passagem e antecessor. Na segunda posição da mesma dimensão a grafo*/
+	for(var i = 0; i < quantVertice; i++){
+		buscaProf[i] = {
+			cor: "branco",
+			adj: lista[i]
+		}
+	}
+
+	
+	while(arrayVertices.length > 0){
+
+		if(buscaProf[verticeEscolhido].cor == "branco"){
+
+			buscaProf[verticeEscolhido].cor = "cinza"
+			
+
+			backtracking.push(verticeEscolhido);
+
+			var verticeSecundario
+
+			while(backtracking.length > 0){
+
+				if(buscaProf[verticeEscolhido].adj.length > 0){
+
+					verticeSecundario = buscaProf[verticeEscolhido].adj.shift().vertice;
+
+					if(buscaProf[verticeSecundario].cor == "branco"){
+
+						buscaProf[verticeSecundario].cor = "cinza"		    					
+						backtracking.push(verticeEscolhido)
+						verticeEscolhido = verticeSecundario
+					}
+
+
+				}
+				else{
+					pilha.unshift(verticeEscolhido)
+					buscaProf[verticeEscolhido].cor = "preto"		    				
+					verticeEscolhido = backtracking.pop()
+
+				}
+			}
+
+		}
+		verticeEscolhido = arrayVertices.shift();
+	}
+	
+	return pilha
+}
     //seters, basicamente modificam a representação, permitindo saber se é lista ou se é matriz
     setRepresentacao(representacao, tp) { 
 		this.rep = representacao
@@ -383,6 +538,36 @@ class Matriz extends Grafo{
 		    this.setRepresentacao(matriz, 0)
 		}
 	}
+	//floyd-warshall
+	getCaminhoMFloyd(){
+		let matriz = this.getRepresentacao()
+		let quantVertice = this.getQNos()
+		
+
+		for(let i = 0; i < quantVertice; i++){
+
+		}
+		for(let i = 0; i < quantVertice; i++) matriz[i][i] = 0
+
+		for(let k = 1; k < quantVertice; k++){
+			for(let i = 1; i < quantVertice; i++){
+				for(let j = 1; j < quantVertice; j++){
+					if((matriz[i][k] != "inf" && matriz[k][j] != "inf") || matriz[i][j] != "inf"){
+						if(matriz[i][j] != "inf"){
+							if(matriz[i][k] + matriz[k][j] < matriz[i][j]) matriz[i][j] = matriz[i][k] + matriz[k][j]
+								
+						}
+						else matriz[i][j] = matriz[i][k] + matriz[k][j]				
+						
+					}
+				}
+			}
+		}
+
+		return matriz
+	}
+
+
  
 }
 
@@ -440,7 +625,65 @@ class Lista extends Grafo {
 			}
 			this.setRepresentacao(lista, 1)
 		}
-    }
+	}
+	getCMGAO(verticeEscolhido){
+		
+		let pilhaTopologica = this.getOrdenacaoTopologica(verticeEscolhido)
+		let quantVertice = this.quantidade
+		let cm = []
+		for(let i = 0; i < quantVertice; i++){
+			let obj = {
+				peso: "inf",
+				pai: null
+			}
+			cm[i] = obj
+		}		
+
+		for(let i = 0; i < quantVertice; i++){
+			for(let j = 0; pilhaTopologica[i].adj[j] != undefined; j++){
+				let vertice = pilhaTopologica[i].vertice
+				let verticeAdj = pilhaTopologica[i].adj[j].vertice
+				
+				if(cm[verticeAdj].peso == "inf" || cm[verticeAdj].peso > pilhaTopologica[i].adj[j].peso){
+					cm[verticeAdj].peso = pilhaTopologica[i].adj[j].peso
+					cm[verticeAdj].pai = vertice
+				}
+			}
+		}
+		return cm
+	}
+	getColoracao(){
+		let lista = this.getRepresentacao()
+		let vertice = this.getVerticeMaiorGrau()
+		let quantVertice = this.quantidade
+		let aux = []
+		for(let i = 0; i < quantVertice; i++){
+			let obj = {
+				cor: 'incolor',
+				adj: lista[i]
+			}
+			aux[i] = obj
+			
+		}
+
+		return this.colorir(aux, vertice)
+
+	}
+	getVerticeMaiorGrau(){
+		let lista = this.getRepresentacao()
+		let maior
+		let valorMaior = 0
+		let quantVertice = this.quantidade
+		for(let i = 0; i < quantVertice; i++){
+			let j = 0
+			for(; lista[i][j] != undefined; j++)
+			if(j > valorMaior) {
+				maior = i
+				valorMaior = j
+			}
+		}
+		return maior
+	}
     getTransposto(){
 		let lista = this.getRepresentacao()
 		let quantVertice = this.getQNos()
@@ -458,7 +701,38 @@ class Lista extends Grafo {
 
 		return transposto
     }
-    
+    colorir(lista, vertice){
+		if(lista[vertice].cor != 'incolor') return lista
+		let cores = ['#7CFF22', '#E8AC0F', '#FF3D1E', '#910FE8', '#0498FF', '#FF118D', '#00FF54', '#E8C60F', '#FF3D1E', '#11FFCB']
+		let contCor = 0
+		let temCor = true
+		while(temCor){
+			temCor = false
+			lista[vertice].cor = cores[contCor]
+			for(let i = 0; lista[vertice].adj[i] != undefined; i++){
+				let verticeEscolhido = lista[vertice].adj[i].vertice
+				if(cores[contCor] == lista[verticeEscolhido].cor) {
+					temCor = true
+					contCor++
+				}
+			}
+		}
+		for(let i = 0; lista[vertice].adj[i] != undefined; i++) this.colorir(lista, lista[vertice].adj[i].vertice)
+		return lista
+	}
+	getReducaoFC(){
+		
+		let transposto = this.getTransposto()
+		let listaTempoBP = this.getOrdenacaoVertices(0)
+		let quantVertice = this.getQNos()
+		let arrayVertices = []
+		
+
+		
+
+		for(let i = 0; i < quantVertice; i++) arrayVertices.push(parseInt(listaTempoBP[i]))
+		return this.getComponents(listaTempoBP, transposto)
+	}
     getComponents(arrayVertices, transposto){
         let lista = transposto
         let quantVertice = this.getQNos()
@@ -661,5 +935,24 @@ getDijkstra(verticeEscolhido){
     }
 
     return dijkstra
+}
+   setConverterMatriz(matriz){
+	   let quantVertice = this.getQNos()
+	   let lista = [quantVertice]
+	    for(var i = 0; i < quantVertice; i++) lista[i] = [];
+
+	    for(let i = 0; i < quantVertice; i++){
+	   	 for(let j = 0; j < quantVertice; j++){
+	   		 if(matriz[i][j] != "inf"){
+	   			 var obj = {
+	   				vertice: j,
+	   				peso: matriz[i][j]
+	   			}
+	   			lista[i].push(obj)
+	   		 }
+	   	 }
+	    }
+	    this.setRep(lista)
+
 }
 }
